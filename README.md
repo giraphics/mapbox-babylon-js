83e3b60c2b27945d0de4a828bcd0a7e8c9317b83
Parminder Singh <you@example.com>
2023-03-23 17:03:34 +0800

    Babylon rendering with Mapbox.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Babylon.js and WebGL2 Mixing
+
+This demo render babylon scene on top of exising webgl2 based on small implementation to test the babylon working of Baylon JS
+ with external webgl2 context.
+
+## Getting started
+
+To run the basic scene:
+
+1. Clone / download this repository
+2. run `npm install` to install the needed dependencies.
+3. run `npm start`
---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
+import { CreateBox, CreateSphere } from "@babylonjs/core";
+import mapboxgl, { Map } from 'mapbox-gl'
+import * as BABYLON from '@babylonjs/core';
+
+/*************************************************/
+/***************** BABYLON SCENE *****************/
+/*************************************************/
+
+let engine: Engine;
+let scene: Scene; 
+
+let customLayer: any;
+const groundElevation = 0.1;
+
+function createEngine(glContext: WebGL2RenderingContext) {
+  return new Engine(
+    glContext,
+    true,
+    {
+    useHighPrecisionMatrix: true // Important to prevent jitter at mercator scale
+    },
+    true
+    );
+}
+
+function createSceneSimple(engine: Engine) {
+	scene = new BABYLON.Scene(engine);
+  scene.activeCamera = new BABYLON.Camera("mapbox-Camera", new BABYLON.Vector3(), scene);
+  scene.autoClear = false;
+  scene.autoClearDepthAndStencil = false;
+  scene.detachControl();
+  
+ 	const light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(1, 1, 0), scene)
+
+  const ground = BABYLON.Mesh.CreateGround('', 50, 50, 3, scene)
+  ground.position.y = groundElevation;
+  const groundmaterial = new BABYLON.StandardMaterial('', scene)
+  ground.material = groundmaterial;
+  groundmaterial.diffuseColor = BABYLON.Color3.FromInts(10, 100, 120)
+
+  const box = BABYLON.MeshBuilder.CreateBox("box", {size:5}, scene);
+  // box.scaling.z = 0.510000123; // HACK TO MAKE THE GROUND VISIBLE
+  // box.position.copyFromFloats(-20, groundElevation, 0)
+  // box.position.y = 1;
+
+  let a = 0;
+  const axis = new BABYLON.Vector3(0, 1, 0);
+  scene.registerBeforeRender(function () {
+    box.rotate(axis, a = 0.01);
+  });
+
+  return scene;	
+}
+
+function getWorldMatrix(mercatorCoordinate: [number, number, number], scaleFactor: number) {
+  const rotationMatrix = BABYLON.Matrix.RotationX(Math.PI / 2);
+  // @ts-ignore
+  const translateMatrix = BABYLON.Matrix.Identity().setTranslationFromFloats(mercatorCoordinate[0], mercatorCoordinate[1], mercatorCoordinate[2]);
+  const scaleMatrix = BABYLON.Matrix.Scaling(scaleFactor, scaleFactor, scaleFactor);
+  const worldMatrix = scaleMatrix.multiply(rotationMatrix.multiply(translateMatrix));
+
+  return worldMatrix;
+}
+
+function renderFromMatrix(matrix:any, mercatorCoordinate: [number, number, number], scaleFactor: number) {
+    const engine = scene.getEngine();
+
+    if (scene) {
+      const projection = BABYLON.Matrix.FromArray(matrix);
+      engine.wipeCaches(false);
+      scene.beforeRender = () => {
+        engine.wipeCaches(true);
+      };
+      if (!scene.activeCamera) {
+        console.log('scene.activeCamera is null')
+        return;
+      }
+      
+      scene.activeCamera.freezeProjectionMatrix(getWorldMatrix(mercatorCoordinate, scaleFactor).multiply(projection));
+      let invert = scene.activeCamera.getProjectionMatrix().clone().invert();
+      scene.activeCamera.position = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(), invert)
+      scene.render(false);
+    }
+  }
+
+/*************************************************/
+/******************* MAP BOX *********************/
+/*************************************************/
+export const mapBoxInit = async (accessToken: string, id: string | HTMLElement, style: string, center: [number, number], zoom: number): Promise<Map> => {
+    mapboxgl.accessToken = accessToken;
+    const map = new mapboxgl.Map({
+        container: id,
+        style: style,
+        zoom: zoom,
+        center: center,
+        pitch: 60,
+        antialias: true,
+      });
+
+      
+  customLayer = {
+    id: '3d-model',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd: function(map:  mapboxgl.Map, gl: WebGL2RenderingContext) {
+       engine = createEngine(gl);
+       scene = createSceneSimple(this.engine);
+    },
+    render(gl: WebGL2RenderingContext, matrix: any) {
+      if (scene) {
+        const modelOrigin = {lng: center[0], lat: center[1]}; // https://docs.mapbox.com/mapbox-gl-js/api/geography/#mercatorcoordinate.fromlnglat
+        const modelAltitude = 0;
+      
+        const mercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude);
+        const scaleFactor = mercatorCoordinate.meterInMercatorCoordinateUnits();
+  
+        renderFromMatrix(matrix, [mercatorCoordinate.x, mercatorCoordinate.y, mercatorCoordinate.z ? mercatorCoordinate.z: 0], scaleFactor)
+      }
+
+      map.triggerRepaint();
+    }
+  }
+
+  return new Promise(resolve => {
+    map.on('style.load', () => { 
+        map.addLayer(customLayer, 'waterway-label'); 
+        resolve(map); 
+    });
+  });
+};
+
+const accessToken = 'pk.eyJ1IjoibW90aW9uYWwiLCJhIjoiY2trejdqcG05MGR0bzJwcGxiYTZweXcxeCJ9.pWj2g2c21CLdwYHAvRj-aA';
+const style = 'mapbox://styles/mapbox/streets-v11';
+
+const mapDiv = document.createElement('div');
+mapDiv.setAttribute('id', 'map')
+mapDiv.style.width = document.body.clientWidth.toString() + 'px';
+mapDiv.style.height = document.body.clientHeight.toString() + 'px';
+document.body.appendChild(mapDiv);
+
+mapBoxInit(accessToken, 'map', style, [103.6958, 1.3542], 17.5).then(() => {
+  // scene started rendering, everything is initialized
+});
+
+
+
